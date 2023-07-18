16225a07a3fb9e81cb7216b779549352e93fdfcf
Luca <luca24ever@gmail.com>
2023-07-18 18:27:36 +0200

    Updates

--- src/pages/Admin/HomeGestioneFarmacisti.js
+++ src/pages/Admin/HomeGestioneFarmacisti.js
@@ -1,6 +1,21 @@
 
 
 function HomeGestioneFarmacisti() {
+const [farmacie, setFarmacie] = useState([]);
+  const navigate = useNavigate();
+    
+  const nuovaFarmacia = (destination) => {
+      navigate(destination);
+  }
+
+  useEffect(() => {
+    axios.get(`http://localhost:3001/homeAdmin/getFarmacie`)
+      .then(res => {
+        setFarmacie(res.data);
+      })
+  }, []);
+
+
   return (
     <div className="HomeGestioneFarmacisti">
       <h1>HomeGestioneFarmacisti</h1>
