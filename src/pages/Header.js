20e5cbae902a2912d9da79a93555ec3a0f4e1f33
fraktc <francy.contib@gmail.com>
2023-07-18 18:51:20 +0200

    button loguout

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -3,7 +3,7 @@
 export default function Header({Logout}) {
     return (
         <div className="header">
-            <h1>FarmaQueue</h1>
+            <h1 className="testoHeader">FarmaQueue</h1>
             <button onClick={Logout}>Logout</button>
         </div>
     )
