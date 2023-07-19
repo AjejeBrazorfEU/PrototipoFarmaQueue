faa2c98290f2afb7cca4e602b6c027efd02cf798
Luca <luca24ever@gmail.com>
2023-07-19 10:29:47 +0200

    updates

--- src/pages/Farmacista/HomeFarmacista.js
+++ src/pages/Farmacista/HomeFarmacista.js
@@ -25,13 +25,27 @@ export default function HomeFarmacista() {
 
 
     return (
-        <div className="">
+        <div className="container">
             <h1>HomeFarmacista</h1>
 
-            <h3>Prossimi Appuntamenti:</h3>
+            <h2>Numero di persone in fila: {prenotazioni.length}</h2>
+
+            <h3>Prossimo Appuntamento da servire:</h3>
 
             <div className="prenotazioniUtente">
-                {prenotazioni.map((prenotazione) => (
+                {prenotazioni.slice(0,1).map((prenotazione) => (
+                    <div className="cardPrenotazione cardNew">
+                        <p>Orario</p>
+                        <h1>{prenotazione.dataEOra}</h1>
+                        <p>Prestazione</p>
+                        <h2>{prenotazione.prestazione}</h2>
+                    </div>
+                ))}
+            </div>
+        
+            <h4>Prossimi Appuntamenti:</h4>
+            <div className="prenotazioniUtente cardFarmacisti">
+                {prenotazioni.slice(1,prenotazioni.length).map((prenotazione) => (
                     <div className="cardPrenotazione">
                         <p>Orario</p>
                         <h1>{prenotazione.dataEOra}</h1>
